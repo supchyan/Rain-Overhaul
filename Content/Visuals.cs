6229ae3ebf4f7f2f5d7a5f6e673d321f60babfcd
supchyan <mikhailsupov@yandex.ru>
2023-10-22 20:28:38 +0300

    Fixes for multiplayer. New icon. WIP

--- Content/Visuals.cs
+++ Content/Visuals.cs
@@ -2,41 +2,39 @@ using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+﻿using System.ComponentModel;
 using Microsoft.Xna.Framework;
 using Terraria.Graphics.Effects;
+using Terraria.ModLoader.Config;
+using Terraria.Localization;
 
 namespace RainOverhaul.Content {
-    public class Visuals : ModSystem {
-
+    public class Visuals:ModSystem {
         private float Intensity;
         private float RainTransition;
         private float Extra;
-        private float worldFactorY;
+        private float WorldFactorY;
 
         public override void PreUpdateTime() {
+            
+            // WORLD PROPS
 
-            if(Main.maxTilesX == 4200)
-            worldFactorY = 4400f;
-            else if(Main.maxTilesX == 6400)
-            worldFactorY = 6600f;
-            else if(Main.maxTilesX == 8400)
-            worldFactorY = 8800f;
+            if(Main.maxTilesX == 4200) WorldFactorY = 4400f;
+            else if(Main.maxTilesX == 6400) WorldFactorY = 6600f;
+            else if(Main.maxTilesX == 8400) WorldFactorY = 8800f;
 
             // RAIN FILTER
 
-            if (Main.netMode != NetmodeID.Server && !Filters.Scene["RainFilter"].IsActive()) 
             Filters.Scene.Activate("RainFilter"); 
 
-            if(Main.netMode != NetmodeID.Server && Filters.Scene["RainFilter"].IsActive()) {
+            if(Filters.Scene["RainFilter"].IsActive()) {
 
                 Tile tile = Main.tile[Main.LocalPlayer.Center.ToTileCoordinates()];
                 bool WallCollision = tile.WallType > WallID.None;
 
                 if(Main.raining && !WallCollision && 
                 !Main.LocalPlayer.ZoneSandstorm && !Main.LocalPlayer.ZoneSnow && 
-                !Main.LocalPlayer.ZoneNormalSpace && Main.LocalPlayer.position.Y < worldFactorY) 
-                {
-
+                !Main.LocalPlayer.ZoneNormalSpace && Main.LocalPlayer.position.Y < WorldFactorY) {
                     RainTransition+=0.01f;
                     if(RainTransition > 1f)
                     RainTransition = 1f;
@@ -44,26 +42,25 @@ namespace RainOverhaul.Content {
                         Main.maxRain = (int)MathHelper.Lerp(0, Main.maxRain, (float)Main.rainTime/1000);
                         Main.maxRaining = MathHelper.Lerp(0, Main.maxRaining, (float)Main.rainTime/1000);
                     }
-
                 }
                 else {
-                    
                     RainTransition-=0.01f;
                     if(RainTransition < 0f)
                     RainTransition = 0f;
-                
                 }
 
-                if(Main.LocalPlayer.ZoneBeach || Main.LocalPlayer.ZoneJungle)
-                Extra = 1.4f;
-                else
-                Extra = 1f;
+                if(Main.LocalPlayer.ZoneBeach || Main.LocalPlayer.ZoneJungle) Extra = 1.4f;
+                else Extra = 1f;
 
-                Intensity = (550 * Main.maxRaining / (20.0f * 645.0f));
+                Intensity = 550 * Main.maxRaining / (20.0f * 645.0f) * ModContent.GetInstance<RainConfig>().Intensity;
 
                 Filters.Scene["RainFilter"].GetShader().UseOpacity(Intensity*RainTransition*Extra).UseIntensity(RainTransition);
-
             }
         }
+    }
+    public class RainConfig:ModConfig {
+		public override ConfigScope Mode => ConfigScope.ClientSide;
+		[DefaultValue(1)]
+		public float Intensity;
     }
 }
--- RainOverhaul.cs
+++ RainOverhaul.cs
@@ -2,7 +2,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Graphics.Effects;
 using Terraria.Graphics.Shaders;
@@ -10,13 +9,9 @@ using Terraria.Graphics.Shaders;
 namespace RainOverhaul {
 	public class RainOverhaul : Mod {
 		public override void Load() {
-			if (Main.netMode != NetmodeID.Server) {
-
-				Ref<Effect> screenRef = new Ref<Effect>(ModContent.Request<Effect>("RainOverhaul/Content/RainFilter", AssetRequestMode.ImmediateLoad).Value);
-				Filters.Scene["RainFilter"] = new Filter(new ScreenShaderData(screenRef, "RainFilter"), EffectPriority.VeryHigh);
-				Filters.Scene["RainFilter"].Load();
-			
-			}
+			Ref<Effect> screenRef = new Ref<Effect>(ModContent.Request<Effect>("RainOverhaul/Content/RainFilter", AssetRequestMode.ImmediateLoad).Value);
+			Filters.Scene["RainFilter"] = new Filter(new ScreenShaderData(screenRef, "RainFilter"), EffectPriority.VeryHigh);
+			Filters.Scene["RainFilter"].Load();
 		}
 	}
 }
